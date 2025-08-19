721fa6e8e2be9387bcf93da47110a62d6b6ff3f0
SinghRaj09 <singh1406raj@gmail.com>
2025-08-19 12:14:36 +0530

    Solution

---
+++ EASY/169. Majority Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size()/2;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto a : mp){
+            if(a.second >n )return a.first;
+        }
+        return -1;
+    }
+};
