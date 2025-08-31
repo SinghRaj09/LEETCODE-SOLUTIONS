449920decd9c4c4a030a8070830ac7a4fdb296bc
SinghRaj09 <singh1406raj@gmail.com>
2025-08-31 13:21:14 +0530

    upload complete

---
+++ MEDIUM/81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	bool search(vector<int>& nums, int target) {
+		int low=0, high=nums.size()-1;
+
+		sort(nums.begin(), nums.end());
+
+		while(low<=high) {
+			int mid = (high+low)/2;
+			if(nums[mid]==target) return true;
+			else if(nums[mid]<target) low = mid+1;
+			else high = mid-1;
+		}
+		return false;
+	}
+};
