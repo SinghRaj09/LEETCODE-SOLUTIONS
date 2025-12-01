0175225c2231bc8287c26d28097e96e6a086ef44
RAJ SINGH <133744177+SinghRaj09@users.noreply.github.com>
2025-12-01 12:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (99.95%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            while (n > 0) {
-                int digit = n % 10;
-                sum += digit * digit;
-                n /= 10;
+        for(int i=0; i<n; i++){
+            int sum=0;
+            while(n>0){
+                int dig=n%10;
+                sum += dig*dig;
+                n/=10;
             }
-            if (sum == 1)
-                return true;
+            if(sum == 1) return true;
             n = sum;
         }
         return false;
