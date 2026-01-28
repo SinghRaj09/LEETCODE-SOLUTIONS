02cb8590b3b29ef7b1de0b3f2df7cc611ce5dd33
RAJ SINGH <133744177+SinghRaj09@users.noreply.github.com>
2026-01-28 12:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (79.00%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        // int count = 0;
+
+        // for(int i=0; i<nums.size(); i++){
+        //     for(int j=0; j<nums.size(); j++){
+        //         if(nums[i]==nums[j] && i<j){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        unordered_map<int, int> pairs;
         int count = 0;
 
-        for(int i=0; i<nums.size(); i++){
-            for(int j=0; j<nums.size(); j++){
-                if(nums[i]==nums[j] && i<j){
-                    count++;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            if (pairs.find(nums[i]) != pairs.end()) {
+                count += pairs[nums[i]];
             }
+            pairs[nums[i]] = pairs[nums[i]] + 1;
         }
-        return count;
+
+        return count; 
     }
 };
